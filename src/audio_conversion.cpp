84e050a6bcb8a2f95a86d3fe376c97b8defde380
Joep Kemperman <mail@joepkemperman.nl>
2016-10-12 20:22:18 +0200

    Simplify and speedup audio conversion

--- src/audio_conversion.cpp
+++ src/audio_conversion.cpp
@@ -2,13 +2,13 @@
 
 StereoAudio mono_to_stereo(const MonoAudio & mono_audio)
 {
+  const size_t doubled_size = mono_audio.samples.size() * 2;
   std::vector<float> doubled_samples;
-  for (float sample : mono_audio.samples)
+  doubled_samples.reserve(doubled_size);
+
+  for (uint32_t i = 0; i < doubled_size; i++)
   {
-    for (int i = 0; i < 2; i++)
-    {
-      doubled_samples.push_back(sample);
-    }
+    doubled_samples.push_back(mono_audio.samples[i / 2]);
   }
   return StereoAudio(doubled_samples, mono_audio.frame_rate);
 }
