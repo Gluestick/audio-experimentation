6837bcc2ca3d066ff88713292cfb99aa0b8bc444
Joep Kemperman <mail@joepkemperman.nl>
2016-10-12 20:22:00 +0200

    Simplify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,10 @@
 #include "audio.hpp"
 #include "audio_conversion.hpp"
-#include <iostream>
-
 
 int main(int argc, char * argv[])
 {
-  std::vector<float> samples = {0.0f, 0.1f, 0.4f, -0.1f, -0.4f, 0.0f};
-
-  std::cout << "Mono" << std::endl;
+  std::vector<float> samples(5e6);
   MonoAudio mono(samples, 22050);
-  std::cout << mono.channel_count << std::endl;
-  std::cout << mono.samples.size() << std::endl;
-  std::cout << mono.frame_rate << std::endl;
-  std::cout << mono.get_sample_rate() << std::endl;
-
-  std::cout << "Stereo" << std::endl;
-  StereoAudio stereo = mono_to_stereo(mono);
-  std::cout << stereo.channel_count << std::endl;
-  std::cout << stereo.samples.size() << std::endl;
-  std::cout << stereo.frame_rate << std::endl;
-  std::cout << stereo.get_sample_rate() << std::endl;
+  mono_to_stereo(mono);
   return 0;
 }
